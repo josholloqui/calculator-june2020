114e343d8f1ea6008e6875f08d36b59f21980797
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-06-25 13:59:16 -0700

    this commit adds full calculator

--- app.js
+++ app.js
@@ -25,3 +25,50 @@ boogerButton.addEventListener('click', () => {
     // update the textContent of the span
     boogerSpan.textContent = sum;
 });
+
+const subInput1 = document.getElementById('sub-input1');
+
+const subInput2 = document.getElementById('sub-input2');
+
+const subButton = document.getElementById('sub-button');
+
+const subEquals = document.getElementById('sub-equals');
+
+subButton.addEventListener('click', () => {
+    const value1 = subInput1.value;
+    const value2 = subInput2.value;
+    const difference = Number(value1) - Number(value2);
+    subEquals.textContent = `= ${difference}`;
+});
+
+//mult = multiplication
+
+const multInput1 = document.getElementById('mult-input1');
+
+const multInput2 = document.getElementById('mult-input2');
+
+const multButton = document.getElementById('mult-button');
+
+const multEquals = document.getElementById('mult-equals');
+
+multButton.addEventListener('click', () => {
+    const value1 = multInput1.value;
+    const value2 = multInput2.value;
+    const xEquals = Number(value1) * Number(value2);
+    multEquals.textContent = `= ${xEquals}`;
+});
+
+const divideInput1 = document.getElementById('divide-input1');
+
+const divideInput2 = document.getElementById('divide-input2');
+
+const divideButton = document.getElementById('divide-button');
+
+const divideEquals = document.getElementById('divide-equals');
+
+divideButton.addEventListener('click', () => {
+    const value1 = divideInput1.value;
+    const value2 = divideInput2.value;
+    const divisionEquals = Number(value1) / Number(value2);
+    divideEquals.textContent = `= ${divisionEquals}`;
+});
